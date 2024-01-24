d4b1d539d933831b389cb3116191fce917bf44ad
Zumrad <z.rizamatova@gmail.com>
2024-01-24 23:08:03 +0100

    shop initial commit

---
+++ src/main/java/de/aittr/g_31_2_shop/services/ProductService.java
@@ -0,0 +1,25 @@
+package de.aittr.g_31_2_shop.services;
+
+import de.aittr.g_31_2_shop.domain.interfaces.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    Product save(Product product);
+    List<Product> getAllActiveProducts();
+    Product getActiveProductById(int id);
+    void update(Product product);
+
+    void deleteById(int id);
+
+    void deleteByName(String name);
+
+    void restoreById(int id);
+
+    int getActiveProductCount();
+
+    double getActiveProductsTotalPrice();
+
+    double getActiveProductAveragePrice();
+}
